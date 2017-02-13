b59016c4c6a4aa1cf266a98f94048c05f4615d4e
payneLee <peinlcy@sina.cn>
2017-02-13 17:29:48 +0800

    #123

---
+++ DynamicProgramming/Best Time to Buy and Sell Stock III.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+    	if (prices.size() <= 1) return 0;
+    	int k = 2;
+        vector<vector<int>> dp(k+1, vector<int>(prices.size(), 0));
+        for (int i = 1; i <= k; ++i) {
+        	int remain = dp[i-1][0] - prices[0];
+        	for (int j = 1; j < prices.size(); ++j) {
+        		dp[i][j] = max(dp[i][j-1], prices[j] + remain);
+        		remain = max(remain, dp[i-1][j] - prices[j]);
+        	}
+        }
+        return dp[k][prices.size()-1];
+    }
+};
