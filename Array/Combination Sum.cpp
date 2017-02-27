7b233076391e235754b74591324a871bf79de58a
payneLee <peinlcy@sina.cn>
2017-02-27 15:25:50 +0800

    #39

---
+++ Array/Combination Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        sort(candidates.begin(), candidates.end());
+        combinationSumHelper(candidates, 0, target, vector<int>(0), res);
+        return res;
+    }
+
+    void combinationSumHelper(vector<int>& candidates, int index, int target, vector<int> tmp, vector<vector<int>>& res) {
+    	for (int i = index; i < candidates.size() && candidates[i] <= target; ++i) {
+    		if (candidates[i] == target) {
+    			tmp.push_back(candidates[i]);
+    			res.push_back(tmp);
+    			return;
+    		} else {
+    			tmp.push_back(candidates[i]);
+    			combinationSumHelper(candidates, i, target - candidates[i], tmp, res);
+    			tmp.pop_back();
+    		}
+    	}
+    }
+};
