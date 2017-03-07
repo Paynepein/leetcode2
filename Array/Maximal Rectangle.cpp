e8bab385aad15c9611bda6cbbc05ee75c24cbb4c
payneLee <peinlcy@sina.cn>
2017-03-07 15:27:20 +0800

    #85

---
+++ Array/Maximal Rectangle.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.size() == 0) return 0;
+        int m = matrix.size(), n = matrix[0].size();
+        int res = 0;
+        vector<int> heights(n+1, 0);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == '1') {
+                    ++heights[j];
+                } else {
+                    heights[j] = 0;
+                }
+            }
+            res = max(res, maximalRectangleHelper(heights));
+        }
+        return res;
+    }
+
+    int maximalRectangleHelper(vector<int>& heights) {
+        stack<int> st;
+        int res = 0;
+        for (int i = 0; i < heights.size(); ++i) {
+            while (!st.empty() && heights[st.top()] > heights[i]) {
+                int h = heights[st.top()];
+                st.pop();
+                int last = st.empty() ? -1 : st.top();
+                res = max(res, h * (i - last -1));
+            }
+            st.push(i);
+        }
+        return res;
+    }
+};
