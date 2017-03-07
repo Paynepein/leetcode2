036148da51aaad3d2c46d38e652288aa5e196392
payneLee <peinlcy@sina.cn>
2017-03-07 15:16:27 +0800

    #81

--- Array/Search in Rotated Sorted Array II.cpp
+++ Array/Search in Rotated Sorted Array II.cpp
@@ -6,19 +6,22 @@ public:
         while (left <= right) {
         	int mid = left + right >> 1;
         	if (nums[mid] == target) return true;
-        	if (target >= nums[0]) {
-        		if (nums[mid] > target) {
+        	if (nums[mid] > nums[right]) {
+        		if (nums[mid] > target && target >= nums[left]) {
         			right = mid-1;
         		} else {
         			left = mid+1;
+        		}
+        	} else if (nums[mid] < nums[right]) {
+        		if (nums[mid] < target && target <= nums[right]) {
+        			left = mid + 1;
+        		} else {
+        			right = mid - 1;
         		}
         	} else {
-        		if (nums[mid] > target) {
-        			right = mid-1;
-        		} else {
-        			left = mid+1;
-        		}
+        		--right;
         	}
         }
+        return false;
     }
 };
