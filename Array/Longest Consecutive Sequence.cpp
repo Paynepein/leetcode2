9a29f9b4798b4df8d42da9156cefade63ac62d83
payneLee <peinlcy@sina.cn>
2017-03-10 16:03:28 +0800

    #126

---
+++ Array/Longest Consecutive Sequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+  		unordered_map<int, int> mp;
+  		int res = 0;
+  		for (int n : nums) {
+  			if (mp.find(n) == mp.end()) {
+  				mp[n] = mp[n - mp[n-1]] = mp[n + mp[n+1]] = mp[n - mp[n-1]] + 1 + mp[n + mp[n+1]];
+  				res = max(res, mp[n]);
+  			}
+  		}      
+  		return res;
+    }
+};
