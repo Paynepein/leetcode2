12826b5dbdf9b8e90ced7d523d6cb4a8c5d63ec1
payneLee <peinlcy@sina.cn>
2017-04-13 11:36:06 +0800

    205

---
+++ HashTable/ Isomorphic Strings.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+  		vector<int> v1(256, 0), v2(256, 0);
+  		for (int i = 0; i < s.size(); ++i) {
+  			if (v1[s[i]] != v2[t[i]]) return false;
+  			v1[s[i]] = v2[t[i]] = i+1;
+  		}
+  		return true;
+    }
+};
