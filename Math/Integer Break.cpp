ccf334dd4a76cff35960022023fc3e9b7f4f5234
payneLee <peinlcy@sina.cn>
2017-04-26 17:35:58 +0800

    343

--- Math/Integer Break.cpp
+++ Math/Integer Break.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        vector<int> dp(n+1, 1);
-        for (int i = 2; i <= n; ++i) {
-        	for (int j = 1; j < i; ++j) {
-        		dp[i] = max(dp[i], max(i-j, dp[i-j]) * j);
-        	}
-        }
-        return dp[n];
+    	if (n < 3) return 1;
+    	else if (n == 3) return 2;
+        else if (n % 3 == 0) return pow(3, n/3);
+        else if (n % 3 == 1) return 4 * pow(3, (n-4)/3);
+        else return 2 * pow(3, n/3);
     }
 };
